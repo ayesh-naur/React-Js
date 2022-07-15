84c4967330652585eb0f8cdf5308bbd616c1c8fd
ayeshanaureen123 <ayesha.naureen@cognizant.com>
2022-07-15 12:45:11 +0000

    react samnple code

---
+++ react-bootcamp/musicmaster/src/components/App.js
@@ -0,0 +1,37 @@
+import React, { Component} from 'react';
+
+class App extends Component{
+    state = {artistQuery: ''};
+
+    updateArtistQuery = event => {
+        this.setState( {artistQuery : event.target.value});
+    }
+
+    handleKeyPress = event => {
+        if(event.key == 'Enter'){
+            this.searchArtist();
+        }
+    }
+
+    searchArtist = () => {
+
+    }
+
+    render() {   
+        return (
+             <div> 
+                 <h2>Music Master</h2>
+                 <input
+                 onChange = {this.updateArtistQuery}
+                 onKeyPress = {this.handleKeyPress}
+                 placeholder='Search for an Artist'
+                 />
+                 <button onClick={this.searchArtist}>Search</button>
+             </div>
+        )
+    }
+  }
+
+
+
+  export default App;
